3726acade7000599286a1aadf67998b95a434a0a
= <=>
2022-11-08 12:25:25 +0000

    login page

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+const NotFound = () => (
+  <div className="container-nt-found">
+    <div className="content-container-nt-found">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/jobby-app-not-found-img.png"
+        alt="not found"
+      />
+      <h1>Page Not Found</h1>
+      <p>we're sorry, the page you requested could not be found</p>
+    </div>
+  </div>
+)
+
+export default NotFound
