476e91922ffb98bdf366d1437e4b2bb12497c2a6
= <=>
2022-11-16 05:57:17 +0000

    pattern

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
-import {Switch, Route, Redirect} from 'react-router-dom'
+import Pattern from './components/Pattern'
 
-import Login from './components/Login'
-import NotFound from './components/NotFound'
-import Home from './components/Home'
-import Jobs from './components/Jobs'
-import ProtectedRoute from './components/ProtectedRoute'
-import DetailedJobItem from './components/DetailedJobItem'
-
-import './App.css'
-
-const App = () => (
-  <Switch>
-    <Route exact path="/login" component={Login} />
-    <ProtectedRoute exact path="/" component={Home} />
-    <ProtectedRoute exact path="/jobs" component={Jobs} />
-    <ProtectedRoute exact path="/jobs/:id" component={DetailedJobItem} />
-    <Route exact path="/not-found" component={NotFound} />
-    <Redirect to="not-found" />
-  </Switch>
-)
+const App = () => <Pattern />
 
 export default App
